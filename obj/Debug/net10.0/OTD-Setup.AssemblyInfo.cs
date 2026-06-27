283fa167d4fe0c008a33b1bf9e46a9e8b7621eb7
Lin_Hou_yu <zhishi_fenzi@hotmail.com>
2026-06-28 02:14:40 +0800

    vmulti驱动自动安装

--- obj/Debug/net10.0/OTD-Setup.AssemblyInfo.cs
+++ obj/Debug/net10.0/OTD-Setup.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OTD-Setup")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdb3eceab2eb24718c63d0a11c3d67adda78fa70")]
 [assembly: System.Reflection.AssemblyProductAttribute("OTD-Setup")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OTD-Setup")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
